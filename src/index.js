8e5ce6c33f1e0a7907a243e569db3aa4a09f8844
Ankur Khandelwal <khandelwalankur176@gmail.com>
2021-07-26 16:25:43 +0530

    Comment added in the index.js file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import NgoSignup from "./pages/NgoSignup/ngoSignup";
 
+//This is the main function
 function Main() {
   return (
     <Router>
