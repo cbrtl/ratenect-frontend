3b9d21f592e073bf7b9dad8f2abb6c3813ec033e
KARTIK SINGH <kumarsinghkartik44@gmail.com>
2021-07-28 19:30:47 +0530

    added bootstrap, login n signup page

--- src/components/homepage/navbar.js
+++ src/components/homepage/navbar.js
@@ -5,14 +5,18 @@ import {Link} from 'react-router-dom';
 export default function Nav(){
     return (
         <nav>
-          <h3>LOGO</h3>
-          <ul className="nav-links">
-            <Link to='/volunteer'><li>Volunteer</li></Link>
-            <Link to='/ngo'><li>NGO</li></Link>
-            <Link to='/blog'><li>Blogs</li></Link>
-            <Link to='/donation'><li>Donation</li></Link>
-            
-          </ul>
+          <Link to='/'><h3>LOGO</h3></Link>
+            <ul  className="nav-links">
+              <Link to='/volunteer'><li>Volunteer</li></Link>
+              <Link to='/ngo'><li>NGO</li></Link>
+              <Link to='/blog'><li>Blogs</li></Link>
+              <Link to='/donation'><li>Donation</li></Link>
+            </ul>
+        
+            <Link to='/login'><button className="login">Log in</button></Link>
+            <Link to='/user-signup'><button className="signup">Sign up</button></Link>
+        
+          
         </nav>
     )
 }
--- src/pages/Homepage/homepage.js
+++ src/pages/Homepage/homepage.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import Nav from '../../components/homepage/navbar';
+import Slider from '../../components/homepage/introslides';
+import Footer from '../../components/homepage/footer';
 import Vol from './volunteer';
 import ngo from './ngo';
 import Blog from './blog';
 import Donation from './donate';
+import Login from './login';
+import UserSignup from '../User/usersignup';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 
 export default function Home(){
     return (
         <Router>
-          <div className="Home">
+          <div>
             <Nav />
             <Switch>
+              <Route path="/" exact component={home}/>
               <Route path="/volunteer" component={Vol}/>
               <Route path="/ngo" component={ngo}/> 
               <Route path="/blog" component={Blog}/> 
               <Route path="/donation" component={Donation}/>
-            </Switch> 
+              <Route path="/login" component={Login}/>
+              <Route path="/user-signup" component={UserSignup}/>
+            </Switch>
           </div>
-        </Router>
+          
+          <Footer />
         
+        </Router>  
     )
+}
+
+const home = () => {
+  return(
+    <Slider />
+
+  )
+  
 }
