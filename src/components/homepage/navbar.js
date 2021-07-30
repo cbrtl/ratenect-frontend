0499047352710c4c4ef518aab1c3b9cc6894a407
zee-p <zesthap@gmail.com>
2021-07-30 22:09:09 +0530

    added login

--- src/components/homepage/navbar.js
+++ src/components/homepage/navbar.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './navbar.css';
 import {Link} from 'react-router-dom';
+import Login_mod from './login.js'
 
 export default function Nav(){
+  // const [show, setShow] = React.useState(false);
+
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
     return (
         <nav>
           <Link to='/'><h3>LOGO</h3></Link>
@@ -13,7 +18,8 @@ export default function Nav(){
               <Link to='/donation'><li>Donation</li></Link>
             </ul>
         
-            <Link to='/login'><button className="login">Log in</button></Link>
+            {/* <button variant="primary" onClick={handleShow} className="login">Log in</button> */}
+            <Login_mod />
             <Link to='/user-signup'><button className="signup">Sign up</button></Link>
         
           
