029c1979ee0104cee809dc86128ff34689975180
KARTIK <kumarsinghkartik44@gmail.com>
2021-08-01 00:01:35 +0530

    Navbar upgraded

--- src/components/homepage/navbar.js
+++ src/components/homepage/navbar.js
@@ -4,25 +4,38 @@ import {Link} from 'react-router-dom';
 import Login_mod from './login.js'
 
 export default function Nav(){
-  // const [show, setShow] = React.useState(false);
-
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
     return (
-        <nav>
-          <Link to='/'><h3>LOGO</h3></Link>
-            <ul  className="nav-links">
-              <Link to='/volunteer'><li>Volunteer</li></Link>
-              <Link to='/ngo'><li>NGO</li></Link>
-              <Link to='/blog'><li>Blogs</li></Link>
-              <Link to='/donation'><li>Donation</li></Link>
-            </ul>
-        
-            {/* <button variant="primary" onClick={handleShow} className="login">Log in</button> */}
-            <Login_mod />
-            <Link to='/user-signup'><button className="signup">Sign up</button></Link>
-        
-          
-        </nav>
+      <header>
+      <Link to='/' style={{ color: '#000', textDecoration: 'none'}}>
+            <h3 className="logo">LOGO</h3>
+      </Link>
+      <input type='checkbox' id='nav-toggle' className='nav-toggle'/>
+      <nav>
+        <ul>
+          <Link to='/volunteer' style={{ color: '#fff', textDecoration: 'none'}}>
+            <li>VOLUNTEER</li>
+          </Link>
+          <Link to='/ngo' style={{ color: '#fff', textDecoration: 'none' }}>
+            <li>NGO</li>
+          </Link>
+          <Link to='/blog' style={{ color: '#fff', textDecoration: 'none' }}>
+            <li>BLOG</li>
+          </Link>
+          <Link to='/donation' style={{ color: '#fff', textDecoration: 'none' }}>
+            <li>DONATION</li>
+          </Link>
+          {/* <Link to='/login' style={{ color: '#fff', textDecoration: 'none' }}>
+            <li className='login'>LOGIN</li>
+          </Link>
+          <Link to='/user-signup' style={{ color: '#fff', textDecoration: 'none' }}>
+            <li className='signup'>SIGN UP</li>
+          </Link> */}
+          <Login_mod />
+        </ul>
+      </nav>
+        <label for='nav-toggle' class='nav-toggle-label'><span></span>
+        </label>
+      
+    </header>
     )
-}
+} 
