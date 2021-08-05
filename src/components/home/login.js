e06a15e5a098dcc639b4ff7b42338667c36ee26f
zee-p <zesthap@gmail.com>
2021-08-05 12:13:53 +0530

    signup and login finalised

--- src/components/home/login.js
+++ src/components/home/login.js
@@ -6,7 +6,7 @@ import Loginpopup from './loginpopup.js';
 export default function Login() {
   const [buttonPopup, setButtonPopup] = useState(false);
   return (
-    <div className="login-popup">
+    <div className="login">
       <main>
         <button onClick={() => setButtonPopup(true)}>Login</button>
       </main>
@@ -66,44 +66,4 @@ export default function Login() {
 
 
 
-
-// export default function LoginMod(){
-
-//   const [show, setShow] = React.useState(false);
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-//   return (
-//     <>
-//       <button variant="primary" onClick={handleShow} className="login">
-//         Log in
-//       </button>
-//       <div className="modal_box">
-//       <Modal show={show} onHide={handleClose} >
-//         <Modal.Header closeButton className="header">
-
-//           <Modal.Title className="titles">LOG IN</Modal.Title>
-
-//         </Modal.Header >
-//         <Modal.Body className="body">
-//           <form>
-//             <input type="email" id="email" name="email" placeholder= "  Email" className="email"></input>
-//             <br></br>
-//             <br></br>
-//             <input type="password" id="password" name="password" placeholder= "  Password" className="pass"></input>
-//           </form>
-//         </Modal.Body>
-
-//         <Modal.Footer className="modal_footer">
-
-//           <button variant="primary" onClick={handleClose} className="getstarted">
-//             Get Started
-//           </button>
-
-//         </Modal.Footer>
-//       </Modal>
-//       </div>
-//     </>
-
-//   )
-// }
 
