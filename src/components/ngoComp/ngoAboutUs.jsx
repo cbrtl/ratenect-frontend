5e4180084f4a949aa4c0a38190111e278e76100f
zee-p <zesthap@gmail.com>
2021-08-18 15:08:15 +0530

    Added- Ngo about us, gallery upload button

--- src/components/ngoComp/ngoAboutUs.jsx
+++ src/components/ngoComp/ngoAboutUs.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import './css/ngoAboutUs.css';
+import profilePic from '../asset/images/profile.png';
+import Slider from '../ImageSlider/ImgSlider';
 
 export default function AboutUsComp() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '50vh',
-      }}
-    >
-      <h3>About Us Component</h3>
+    <div className="about-us-box">
+      <div className="profile">
+        <div className="image-container">
+          <img src={profilePic} alt="logo" />
+        </div>
+        <div className="name-container">
+          <h6>Rahul Maheswari</h6>
+          <p>Founder</p>
+        </div>
+      </div>
+      <div className="profile">
+        <div className="image-container">
+          <img src={profilePic} alt="logo" />
+        </div>
+        <div className="name-container">
+          <h6>Rahul Maheswari</h6>
+          <p>Secretary </p>
+        </div>
+      </div>
+      <div className="profile">
+        <div className="image-container">
+          <img src={profilePic} alt="logo" />
+        </div>
+        <div className="name-container">
+          <h6>Rahul Maheswari</h6>
+          <p>Finance Trustee</p>
+        </div>
+      </div>
+      <div className="slider">
+        <Slider />
+      </div>
     </div>
   );
 }
