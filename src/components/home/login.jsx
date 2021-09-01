9d4cc12f3859396b102ba7ced81f28c998bd3e50
Ankur Khandelwal <khandelwalankur176@gmail.com>
2021-09-01 08:42:31 +0530

    Added axios to Login (#40)

--- src/components/home/login.jsx
+++ src/components/home/login.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from 'react';
 // import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
 import './css/login.css';
 import Loginpopup from './loginpopup';
 
 export default function Login() {
   const [buttonPopup, setButtonPopup] = useState(false);
+  const [userForm, setUserForm] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function submitLogin() {
+    const data = { email, password };
+    console.log(data);
+    const url = userForm ? '/api/userlogin' : '/api/ngologin';
+    axios.post(url, data).then((res) => {
+      if (res.status === 201) {
+        setButtonPopup(false);
+        setUserForm(false);
+        setEmail('');
+        setPassword('');
+      } else {
+        setButtonPopup(true);
+      }
+      console.log(res);
+    });
+  }
+
   return (
     <div className="login">
       <main>
@@ -12,16 +34,26 @@ export default function Login() {
       </main>
 
       <Loginpopup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h2>Log in</h2>
+        <div className="switch">
+          <button onClick={() => setUserForm(false)}>NGO</button>
+          <button onClick={() => setUserForm(true)}>User</button>
+        </div>
         <form>
-          <h2>Log in</h2>
           <div className="form-element">
-            <input type="text" name="Enter email" placeholder="Email" />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="form-element">
             <input
               type="password"
-              name="Enter password"
+              name="password"
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="form-element">
@@ -29,7 +61,7 @@ export default function Login() {
             <label>Remember me</label>
           </div>
           <div className="form-element">
-            <button>Get Started!</button>
+            <button onClick={submitLogin}>Get Started!</button>
           </div>
           <div className="form-element">
             <a href="/">Forgot password?</a> {/* Link will be changed */}
